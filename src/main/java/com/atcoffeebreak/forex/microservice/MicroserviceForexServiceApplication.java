1d4fd9b54a5b133cc05fcbaca3b3301ca76ecf6b
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2019-04-17 22:40:59 +0200

    initial commit

---
+++ src/main/java/com/atcoffeebreak/forex/microservice/MicroserviceForexServiceApplication.java
@@ -0,0 +1,13 @@
+package com.atcoffeebreak.forex.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceForexServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceForexServiceApplication.class, args);
+    }
+
+}
